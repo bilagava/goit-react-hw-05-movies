2ee563b411586bff8a2e2efc8810fdc417cb5969
Yuriy Brovko <ysb.ptc@gmail.com>
2022-10-10 10:58:05 +0300

    basename

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -3,21 +3,23 @@ import css from './AppBar.module.css';
 
 export default function AppBar() {
   return (
-    <header>
-      <nav className={css.navBox}>
-        <NavLink
-          to="/"
-          className={({ isActive }) => (isActive ? css.active : css.btn)}
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/movies"
-          className={({ isActive }) => (isActive ? css.active : css.btn)}
-        >
-          Movies
-        </NavLink>
-      </nav>
-    </header>
+    <>
+      <header>
+        <nav className={css.navBox}>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? css.active : css.btn)}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/movies"
+            className={({ isActive }) => (isActive ? css.active : css.btn)}
+          >
+            Movies
+          </NavLink>
+        </nav>
+      </header>
+    </>
   );
 }
